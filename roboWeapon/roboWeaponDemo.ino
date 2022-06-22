84822cfe3eb922dbe68cea3f6a7bd075f193d041
Lucas Raoni <lucasraoniha@gmail.com>
2022-06-21 22:47:24 -0300

    fixed variable and overflow of variables

--- roboWeapon/roboWeaponDemo.ino
+++ roboWeapon/roboWeaponDemo.ino
@@ -61,7 +61,6 @@ void loop() {
     }
     Serial.println("A arma sendo ativada");
     weaponStep++;
-    //after weapon being all pushed
     if (weaponStep == 200) {
       Serial.println("A arma foi ativada");
       weaponState = 1;
@@ -73,7 +72,6 @@ void loop() {
 
   if (weaponState == 1 && millis() - startTime > 5000) {
     Serial.printlnI("Arma esta sendo desativada");
-    //after weapon being all pushed
     weaponStep++;
     if (weaponStep == 200) {
       Serial.printlnI("Arma foi desativada");
